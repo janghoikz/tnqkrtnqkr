7f5aa2ac6f098eb72a964d384959b740c295fb85
ohne <ghdlr785@gmail.com>
2022-04-30 23:11:01 +0900

    초기 커밋.

---
+++ tnqkrtnqkr/tnqkrtnqkr.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main()
+{
+    int num{};
+    std::cout << "숫자를 입력하세요" << std::endl;
+    std::cin >> num;
+    std::string a = "수";
+    std::string b = "박";
+
+    std::string ans{};
+
+    for (int i=0; i < num; i++)
+    {
+        if ((i % 2) == 0)
+        {
+            ans += a;
+        }
+        else
+        {
+            ans += b;
+        }
+        
+    }
+    std::cout << ans << std::endl;
+}
+
